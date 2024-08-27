202564104090db90c2a7ce6c8f15a20033494ee1
Pablo Estigarribia <pablodav@gmail.com>
2024-08-26 22:56:48 -0300

    more tabula tests

--- tabula_extract.py
+++ tabula_extract.py
@@ -1,7 +1,7 @@
 from tabula import read_pdf
 
 # Specify the path to your PDF file
-pdf_file = '../data/res_731_2122.pdf'
+pdf_file = 'data/res_731_2122.pdf'
 
 # Use the read_pdf function to extract tables
 tables = read_pdf(pdf_file, pages='all')
