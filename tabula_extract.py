54bc77a56588e510480bac7459811f7b9ac46f5a
Pablo Estigarribia <pablodav@gmail.com>
2024-09-09 22:46:50 -0300

    extract3

--- tabula_extract.py
+++ tabula_extract.py
@@ -1,7 +1,8 @@
 from tabula import read_pdf
 
 # Specify the path to your PDF file
-pdf_file = 'data/res_731_2122.pdf'
+# pdf_file = 'data/res_731_2122.pdf'
+pdf_file = 'data/res_731_2125.pdf'
 
 # Use the read_pdf function to extract tables
 tables = read_pdf(pdf_file, pages='all')
@@ -19,4 +20,4 @@ for i, table in enumerate(tables):
     print("\n")
 
     # Save the table to a CSV file for future analysis (if required)
-    table.to_csv(f"data/extract2/table_{i+1}.csv", index=False)
+    table.to_csv(f"data/extract3/table_{i+1}.csv", index=False)
